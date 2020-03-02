87a1703a563949630c572741605687b4bdaab080
cuongbuitomosia <cuong.bui@tomosia.com>
2020-03-02 09:26:02 +0700

    Add eslint and prettier packages with airbnb base config to improve code base and enforce coding style

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-'use strict';
+const QRCode = require("./easyqrcodejs-nodejs-extended/");
 
-const QRCode = require('./easyqrcodejs-nodejs-extended/');
-
-exports.handler = async (event, context, callback) => {
+exports.handler = async event => {
   const options = event.queryStringParameters;
-  const token = event.queryStringParameters.token;
-  delete options.token
+  const { token } = event.queryStringParameters;
+  console.log(token);
+  delete options.token;
   let response = null;
   const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true
   };
 
-  if (token === 'b57b22fd7a5979aa14939f56ac95af4297552addc46835cc695ec8f29377c0290148b') {
+  if (token === "b57b22fd7a5979aa14939f56ac95af4297552addc46835cc695ec8f29377c0290148b") {
     const qrcode = new QRCode(options);
-    const data = await qrcode.toDataURL();
+    const data = await qrcode.saveImage({ path: "q.png" });
     response = {
       statusCode: 200,
       headers: headers,
@@ -24,9 +23,10 @@ exports.handler = async (event, context, callback) => {
     let errorMessage = null;
 
     if (token) {
-      errorMessage = 'The access token is invalid. You are not authorized to use this service';
+      errorMessage = "The access token is invalid. You are not authorized to use this service";
     } else {
-      errorMessage = 'The access token is not provided. Please provide a valid access token to use this';
+      errorMessage =
+        "The access token is not provided. Please provide a valid access token to use this service";
     }
 
     response = {
